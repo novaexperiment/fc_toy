5a6d79833f48df4faba7fcb06ecb817af33c742c
Christopher Backhouse <bckhouse@fnal.gov>
2020-11-09 18:53:34 +0000

    Add ability to use FC critical values, and to plot critical values.

--- fc.cxx
+++ fc.cxx
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <vector>
@@ -18,6 +19,8 @@ std::vector<double> DeltaScanValues()
   return ret;
 }
 
+const std::vector<double> kDeltaScanValues = DeltaScanValues();
+
 double Nexp(double delta)
 {
   return A-B*sin(delta)+C; // hardcoded normal hierarchy here
@@ -35,9 +38,9 @@ double gaus(double x, double mu)
   return exp(-(x-mu)*(x-mu)/(2*sigmasq))/sqrt(2*M_PI*sigmasq);
 }
 
-int main()
+// Precompute what the best chisq would be for each number of observed events
+std::vector<double> precompute_chisq_best()
 {
-  // Precompute what the best chisq would be for each number of observed events
   std::vector<double> chisq_best(Nmax*invstep);
 
   for(int i = 0; i < Nmax*invstep; ++i){
@@ -49,11 +52,68 @@ int main()
     else chisq_best[i] = chisq(Nobs, A+B+C);
   }
 
-  const double dchisq_crit = 1;
+  return chisq_best;
+}
 
-  std::cout << "delta_true\tcoverage" << std::endl;
+std::vector<double> wilks_critical_values()
+{
+  return std::vector<double>(kDeltaScanValues.size(), 1);
+}
 
-  for(double delta_true: DeltaScanValues()){
+std::vector<double> fc_critical_values(const std::vector<double>& chisq_best)
+{
+  std::vector<double> dchisq_crit(kDeltaScanValues.size());
+
+  for(int j = 0; j < kDeltaScanValues.size(); ++j){
+    const double delta_true = kDeltaScanValues[j];
+
+    struct Expt{
+      Expt(double d, double p) : dchisq(d), prob(p) {}
+      bool operator<(const Expt& e) const {return dchisq < e.dchisq;}
+      double dchisq;
+      double prob;
+    };
+
+    std::vector<Expt> expts;
+
+    for(int i = 0; i < Nmax*invstep; ++i){
+      const double Nobs = i/double(invstep);
+
+      // Probablility of observing Nobs given delta_true
+      const double prob = gaus(Nobs, Nexp(delta_true))/double(invstep);
+
+      const double chisq_true = chisq(Nobs, Nexp(delta_true));
+
+      const double dchisq = chisq_true - chisq_best[i];
+
+      expts.emplace_back(dchisq, prob);
+    } // end for Nobs (i)
+
+    std::sort(expts.begin(), expts.end()); // from low to high dchisq
+
+    double cov = 0;
+    double crit = 0;
+    for(const Expt& e: expts){
+      cov += e.prob;
+      if(cov > .6827){
+        crit = e.dchisq; // TODO interpolate to previous value?
+        break;
+      }
+    } // end for e
+    dchisq_crit[j] = crit;
+  } // end for delta_true (j)
+
+  return dchisq_crit;
+}
+
+std::vector<double> evaluate_coverage(const std::vector<double>& dchisq_crit,
+                                      const std::vector<double>& chisq_best)
+{
+  std::vector<double> cov(kDeltaScanValues.size());
+
+  for(int j = 0; j < kDeltaScanValues.size(); ++j){
+    const double delta_true = kDeltaScanValues[j];
+
     double coverage = 0;
 
     for(int i = 0; i < Nmax*invstep; ++i){
@@ -66,9 +126,52 @@ int main()
 
       const double dchisq = chisq_true - chisq_best[i];
 
-      if(dchisq <= dchisq_crit) coverage += prob;
+      if(dchisq <= dchisq_crit[j]) coverage += prob;
     } // end for Nobs
 
-    std::cout << delta_true << "\t" << 100*coverage << std::endl;
+    cov[j] = coverage;
+  } // end for delta_true
+
+  return cov;
+}
+
+enum EMethod{
+  kInvalid,
+  kWilks,
+  kFC
+};
+
+int main(int argc, char** argv)
+{
+  EMethod method = kInvalid;
+  if(argc > 1){
+    if(std::string_view(argv[1]) == "wilks") method = kWilks;
+    if(std::string_view(argv[1]) == "fc") method = kFC;
+  }
+
+  if(method == kInvalid){
+    std::cerr << "Usage: fc METHOD" << std::endl
+              << "  METHOD: 'wilks' or 'fc'" << std::endl;
+    return 1;
+  }
+
+  // Precompute what the best chisq would be for each number of observed events
+  const std::vector<double> chisq_best = precompute_chisq_best();
+
+  std::cerr << "Computing critical values..." << std::endl;
+
+  std::vector<double> dchisq_crit;
+  if(method == kWilks) dchisq_crit = wilks_critical_values();
+  if(method == kFC) dchisq_crit = fc_critical_values(chisq_best);
+
+  std::cerr << "Evaluating coverage..." << std::endl;
+
+  const std::vector<double> coverage = evaluate_coverage(dchisq_crit, chisq_best);
+
+  std::cout << "delta_true\tcoverage\tdchisq_crit" << std::endl;
+
+  for(int j = 0; j < kDeltaScanValues.size(); ++j){
+    const double delta_true = kDeltaScanValues[j];
+    std::cout << delta_true << "\t" << 100*coverage[j] << "\t" << dchisq_crit[j] << std::endl;
   } // end for delta_true
 }
--- plot.C
+++ plot.C
@@ -6,30 +6,45 @@
 
 std::string junks;
 
-void plot(std::string fname, std::string outname = "")
+void plot(std::string fname, std::string covname = "", std::string critname = "")
 {
   std::ifstream fin(fname);
 
-  fin >> junks >> junks; // header line
+  fin >> junks >> junks >> junks; // header line
 
-  TGraph* g = new TGraph;
+  TGraph* gcov = new TGraph;
+  TGraph* gcrit = new TGraph;
 
   while(fin){
-    double delta, coverage;
-    fin >> delta >> coverage;
+    double delta, coverage, crit;
+    fin >> delta >> coverage >> crit;
 
-    g->SetPoint(g->GetN(), delta, coverage);
+    gcov->SetPoint(gcov->GetN(), delta, coverage);
+    gcrit->SetPoint(gcrit->GetN(), delta, crit);
   }
 
   (new TH2F("", ";#delta_{true};Coverage (%)", 100, 0, 2*M_PI, 100, 50, 100))->Draw();
 
-  g->SetLineWidth(2);
-  g->Draw("l same");
+  gcov->SetLineWidth(2);
+  gcov->Draw("l same");
 
   TLine* lin = new TLine(0, 68.27, 2*M_PI, 68.27);
   lin->SetLineWidth(2);
   lin->SetLineStyle(7);
   lin->Draw();
 
-  if(!outname.empty()) gPad->Print(outname.c_str());
+  if(!covname.empty()) gPad->Print(covname.c_str());
+
+
+  (new TH2F("", ";#delta_{true};#Delta#chi^{2}_{crit}", 100, 0, 2*M_PI, 100, 0, 2))->Draw();
+
+  gcrit->SetLineWidth(2);
+  gcrit->Draw("l same");
+
+  lin = new TLine(0, 1, 2*M_PI, 1);
+  lin->SetLineWidth(2);
+  lin->SetLineStyle(7);
+  lin->Draw();
+
+ if(!critname.empty()) gPad->Print(critname.c_str());
 }
