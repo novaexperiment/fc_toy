63e76cd98d3ef355e924b9315780c29f4c7ff288
Christopher Backhouse <bckhouse@fnal.gov>
2021-04-08 14:54:48 +0100

    Update comparisons plot macro to new filenames, produce another overlay plot variant.

--- compare.C
+++ compare.C
@@ -60,14 +60,15 @@ void compare()
     c->cd(i+1);
 
     std::string meth;
+    std::string suffix;
     int col;
-    if(i == 0){meth = "wilks"; col = kBlack;}
-    if(i == 1){meth = "fc"; col = kRed;}
-    if(i == 2){meth = "hc"; col = kGreen+2;}
-    if(i == 3){meth = "prof"; col = kBlue;}
+    if(i == 0){meth = "wilks"; col = kBlack;   suffix = "_either";}
+    if(i == 1){meth = "fc";    col = kRed;     suffix = "_either_nh";}
+    if(i == 2){meth = "hc";    col = kGreen+2; suffix = "_either";}
+    if(i == 3){meth = "prof";  col = kBlue;}
 
-    TGraph* gcov_nh = load_coverage(meth+"_nh_either_nh.txt", col);
-    TGraph* gcov_ih = load_coverage(meth+"_ih_either_nh.txt", col, 7);
+    TGraph* gcov_nh = load_coverage(meth+"_nh"+suffix+".txt", col);
+    TGraph* gcov_ih = load_coverage(meth+"_ih"+suffix+".txt", col, 7);
 
     (new TH2F("", (meth+";#delta_{true};Coverage (%)").c_str(), 100, 0, 2*M_PI, 100, 0, 100))->Draw();
 
@@ -76,7 +77,7 @@ void compare()
   }
 
   c->cd(0);
-  gPad->Print("comp_cov.png");
+  gPad->Print("comp_cov.pdf");
 
   /*
   TLine* lin = new TLine(0, 68.27, 2*M_PI, 68.27);
@@ -86,30 +87,53 @@ void compare()
   */
 
   new TCanvas;
+  (new TH2F("", ";#delta_{true};Coverage (%)", 100, 0, 2*M_PI, 100, 30, 100))->Draw();
+  TGraph* gcov_wilks_nh = load_coverage("wilks_nh_either.txt", kBlack);
+  TGraph* gcov_wilks_ih = load_coverage("wilks_ih_either.txt", kBlack, 7);
+  TGraph* gcov_fc_nh = load_coverage("fc_nh_either_nh.txt", kRed);
+  TGraph* gcov_fc_ih = load_coverage("fc_nh_either_ih.txt", kRed, 7);
+
+  TGraph* target = new TGraph;
+  target->SetPoint(0, 0, 68.27);
+  target->SetPoint(1, 2*M_PI, 68.27);
+  target->SetLineStyle(2);
+  target->Draw("l same");
+
+  gcov_wilks_nh->Draw("l same");
+  gcov_wilks_ih->Draw("l same");
+  gcov_fc_nh->Draw("l same");
+  gcov_fc_ih->Draw("l same");
+
+  gPad->Print("comp_cov_overlay_basic.pdf");
+
+  new TCanvas;
   (new TH2F("", ";#delta_{true};Coverage (%)", 100, 0, 2*M_PI, 100, 50, 90))->Draw();
-  TGraph* gcov_hc_nh = load_coverage("hc_nh_either_nh.txt", kGreen+2);
-  TGraph* gcov_hc_ih = load_coverage("hc_ih_either_nh.txt", kGreen+2, 7);
-  TGraph* gcov_prof_nh = load_coverage("prof_nh_either_nh.txt", kBlue);
-  TGraph* gcov_prof_ih = load_coverage("prof_ih_either_nh.txt", kBlue, 7);
+  TGraph* gcov_hc_nh = load_coverage("hc_nh_either.txt", kGreen+2);
+  TGraph* gcov_hc_ih = load_coverage("hc_ih_either.txt", kGreen+2, 7);
+  TGraph* gcov_prof_nh = load_coverage("prof_nh.txt", kBlue);
+  TGraph* gcov_prof_ih = load_coverage("prof_ih.txt", kBlue, 7);
+
+  target->Draw("l same");
 
   gcov_hc_nh->Draw("l same");
   gcov_hc_ih->Draw("l same");
   gcov_prof_nh->Draw("l same");
   gcov_prof_ih->Draw("l same");
 
-  gPad->Print("comp_cov_overlay.png");
+  gPad->Print("comp_cov_overlay.pdf");
 
   new TCanvas;
-  TGraph* gwilks = load_crit("wilks_nh_either_nh.txt", kBlack);
+  TGraph* gwilks = load_crit("wilks_nh_either.txt", kBlack);
   TGraph* gfcnh = load_crit("fc_nh_either_nh.txt", kRed);
   TGraph* gfcih = load_crit("fc_nh_either_ih.txt", kRed, 7);
-  TGraph* ghc = load_crit("hc_nh_either_nh.txt", kGreen+2);
+  TGraph* ghc = load_crit("hc_nh_either.txt", kGreen+2);
 
   (new TH2F("", ";#delta_{true};Critical value", 100, 0, 2*M_PI, 100, 0, 2))->Draw();
+
   gwilks->Draw("l same");
   gfcnh->Draw("l same");
   gfcih->Draw("l same");
   ghc->Draw("l same");
 
-  gPad->Print("comp_crit.png");
+  gPad->Print("comp_crit.pdf");
 }
